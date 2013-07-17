5b9086cef58cb6edf73f20ca266b4bc7387b47de
wowe <woebookpro@gmail.com>
2013-07-17 14:34:12 +0700

    create first book posts

---
+++ books/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Books::Application.config.secret_key_base = '9a727e3c2cb07d625cd41214202c18dd5089c623fd381661059a5964291a0e16d1f1d96cf66fdbb0305234dec8fe839e8ada372b78d6f9dd521e34b54b3c4b0b'
