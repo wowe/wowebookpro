a692e08af92bd60abdd447f31c41320c9c663222
wowe <woebookpro@gmail.com>
2013-08-06 10:35:34 +0700

    new project and new template

---
+++ books/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Books::Application.config.secret_key_base = '3000d200a2caa51924217f2fb3b848f1f94cc872b05232f4aae0045a1122ab3cc746df31379dabd623c67867ac43033a4145060b68ee2ab3e6a2b68c37bbd3fd'
