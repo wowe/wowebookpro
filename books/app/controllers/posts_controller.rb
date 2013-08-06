eb5e942796ccde3a94902612439820a911e2d81b
wowe <woebookpro@gmail.com>
2013-08-06 10:28:17 +0700

    starting over

--- books/app/controllers/posts_controller.rb
+++
@@ -1,8 +0,0 @@
-class PostsController < ApplicationController
-    def new
-    end
-
-    def create
-        render text: params[:post].inspect
-    end
-end
--- books/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Books::Application.config.session_store :cookie_store, key: '_books_session'
